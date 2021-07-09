0cca3ff743376bedfdb0a687b8a620801e112c9c
Marco Ferrati <marco.ferrati@gmail.com>
2021-07-09 12:44:06 +0200

    Update to 1.4.0, see changelog on GitLab for details

--- asciimoji.rb
+++ asciimoji.rb
@@ -1,8 +1,8 @@
 class Asciimoji < Formula
   desc "Program to search asciimoji saved in a local, editable file"
   homepage "https://gitlab.com/jjocram/asciimoji"
-  url "https://gitlab.com/jjocram/asciimoji/-/archive/v1.3.0/asciimoji-v1.3.0.tar.gz"
-  sha256 "ec99a768e394e5c32f1f7ab02ffa473d2930d57f4e530b9ee2476f666acbbdd0"
+  url "https://gitlab.com/jjocram/asciimoji/-/archive/v1.4.0/asciimoji-v1.4.0.tar.gz"
+  sha256 "893e9c2179dc7d47e7f29f08b61b89440fde4221ace99aa60c5325da058bb9ec"
   license "GPL-3.0-or-later"
 
   depends_on "rust" => :build
